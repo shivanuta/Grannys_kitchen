1f793490a0ccecafe6699c14a251e2f89e26872b
Shiva Rama Krishna Nutakki <nutakki.shivaramakrishna99@gmail.com>
2022-05-16 11:02:32 -0400

    Core WebApp Added.

---
+++ GrannysKitchen.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrannysKitchen.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

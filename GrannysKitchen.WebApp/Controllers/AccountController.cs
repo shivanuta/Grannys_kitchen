3f7f8a476abb9d6e2f1432fc6574077e853ae5ae
kavya-collab <kavyach23@gmail.com>
2022-06-07 03:04:45 -0400

    logout implemented

--- GrannysKitchen.WebApp/Controllers/AccountController.cs
+++ GrannysKitchen.WebApp/Controllers/AccountController.cs
@@ -68,5 +68,46 @@ namespace GranysKitchen.WebApp.Controllers
         }
 
 
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> ChefLoginCheck(AuthenticateRequest user)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+                string endpoint = apiBaseUrl + "Account/ChefAuthenticate";
+
+                using (var Response = await client.PostAsync(endpoint, content))
+                {
+                    var apiResponse = await Response.Content.ReadAsStringAsync();
+                    var authenticateResponse = JsonConvert.DeserializeObject<ChefAuthenticateResponse>(apiResponse);
+
+                    if (authenticateResponse != null && authenticateResponse.ResponseMesssage.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        HttpContext.Session.SetString("Token", authenticateResponse.Token);
+                        HttpContext.Session.SetInt32("IsChef", 1);
+                        HttpContext.Session.SetString("Username", authenticateResponse.Username);
+                        HttpContext.Session.SetString("Name", authenticateResponse.FirstName + " " + authenticateResponse.LastName);
+                        HttpContext.Session.SetInt32("UserId", authenticateResponse.Id);
+                        TempData["Profile"] = JsonConvert.SerializeObject(apiResponse);
+                        return RedirectToAction("Index", "Chef");
+                    }
+                    else
+                    {
+                        ModelState.Clear();
+                        ModelState.AddModelError(string.Empty, "Username or Password is Incorrect");
+                        return View("Index");
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
+
